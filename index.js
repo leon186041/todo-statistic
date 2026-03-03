9918f8c1e7f62350721f1fefad66c42fdd3cb2f9
Lena <schanina06@gmail.com>
2026-03-03 20:13:52 +0500

    meto fix

--- index.js
+++ index.js
@@ -12,10 +12,39 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    const regex = /\/\/ TODO .+/;
-    switch (command) {
+    const files = getFiles();
+    const regex = /\/\/ TODO (.+)/g;
+    let todo = [];
+    for (const file of files) {
+        todo.push(...getTodoText(file, regex));
+    }
+    switch (command.split(' ')[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (const to of todo)
+                console.log(to);
+            break;
+        case 'important':
+            for (const to of todo) {
+                if (to.indexOf('!') !== -1) {
+                    console.log(to);
+                }
+            }
+            break;
+        case 'user':
+            const user = command.split(' ')[1];
+            for (const to of todo) {
+                if (to.indexOf(';') !== -1) {
+                    const parseStr = to.split(';');
+                    if (command.split(' ')[1] === parseStr[0].slice(8)) {
+                        const com = parseStr[parseStr.length - 1];
+                        console.log(com);
+                    }
+
+                }
+            }
             break;
         default:
             console.log('wrong command');
@@ -24,7 +53,7 @@ function processCommand(command) {
 }
 
 function getTodoText(str, regex) {
-    return str.match(regex)[0];
+    return str.match(regex);
 }
 
 // TODO you can do it!
